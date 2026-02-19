b90bf9d427e83916c61ec68b67bcdb35ae323342
Héctor Alonso <healhovi@gmail.com>
2026-02-19 07:31:14 -0300

    Definimos una pseudo-estructura al documento (ver outline)

--- time_series_analysis.py
+++
@@ -1,7 +0,0 @@
-lista = ['Héctor','Alonso','Hormazábal','Vildósola']
-print(lista)
-
-for i in range(len(lista)):
-    lista_invertida.append(lista[-i-1])
-    print(lista[-i-1]) 
-print(lista_invertida)
